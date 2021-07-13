9081c74242aa7275bcfe8140ce9dabf71ee7be43
phancongchinh <phancongchinhpqa@gmail.com>
2021-07-13 08:23:26 +0700

    Commit lần thứ hai

--- myscript.js
+++ myscript.js
@@ -1,20 +1,25 @@
 function bmi() {
-    let w = parseFloat(document.getElementById("weight").value);
-    let h = parseFloat(document.getElementById("height").value);
+    let inputw = document.getElementById("weight").value;
+    let inputh = document.getElementById("height").value;
 
-
-    if ((w <= 0) || (h <= 0)) {
+    if ((inputw === "") || (inputh === "")) {
         document.getElementById("result").innerHTML = "Input invalid.";
     } else {
-        let result = (w / (Math.pow(h,2))).toFixed(2);
-        if (result < 18) {
-            document.getElementById("result").innerHTML = "Underweight: " + result;
-        } else if (result < 25.0) {
-            document.getElementById("result").innerHTML = "Normal: " + result;
-        } else if (result < 30.0) {
-            document.getElementById("result").innerHTML = "Overweight: " + result;
+        let w = parseFloat(inputw);
+        let h = parseFloat(inputh);
+        if ((w <= 0) || (h <= 0)) {
+            document.getElementById("result").innerHTML = "Input invalid.";
         } else {
-            document.getElementById("result").innerHTML = "Obese: " + result;
+            let result = (w / (Math.pow(h,2))).toFixed(2);
+            if (result < 18) {
+                document.getElementById("result").innerHTML = "Underweight: " + result;
+            } else if (result < 25.0) {
+                document.getElementById("result").innerHTML = "Normal: " + result;
+            } else if (result < 30.0) {
+                document.getElementById("result").innerHTML = "Overweight: " + result;
+            } else {
+                document.getElementById("result").innerHTML = "Obese: " + result;
+            }
         }
     }
 }
